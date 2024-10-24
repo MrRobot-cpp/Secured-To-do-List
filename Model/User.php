3c0467b01d92e7c707d8a05d4b17a392576b4b14
LordZorg58 <117113871+LordZorg58@users.noreply.github.com>
2024-10-24 18:34:34 +0300

    Update User.php
    get_id function that retrieve the user id by the email

--- Model/User.php
+++ Model/User.php
@@ -57,5 +57,22 @@ class User {
     
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+    public function get_id( $email) {
+    try {
+    
+        $query = "SELECT id FROM USERS WHERE email = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$email,$password]);
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        
+        
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        return null;
+    }
+}
+
 }
 ?>
